59bd5a08792a00a3296a04e8c61a8d6212ffe0d3
C.Y. Tang <chuanyou.tang@gmail.com>
2014-12-16 08:29:13 -0500

    parse search result from yelp

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -29,9 +29,23 @@ class SearchController extends Zend_Controller_Action
 
     public function searchAction()
     {
+        $result = array();
+
         $term = $this->_getParam('term');
         $location = $this->_getParam('location');
-        $result = $this->search($term, $location);
+        $yelp_json = $this->search($term, $location);
+
+        $yelp_array = Zend_Json::decode($yelp_json);
+        $yelp_businesses = $yelp_array['businesses'];
+        
+        foreach ($yelp_businesses as $business) :
+            $shop_id = business['id'];
+            $name = business['name'];
+            $image_url = business['image_url'];
+            $business_map = array('shop_id' => $shop_id,
+                                  'name' => $name,
+                                  'image_url' => $image_url);
+            $result[] = $business_map;
 
         echo Zend_Json::encode(array('term'=>$term, 'location'=>$location, 'result'=>$result));
         exit();
