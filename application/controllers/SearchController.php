e44c9f6bb2a4712ff2c1aeeb53c193ec2a58e07e
C.Y. Tang <chuanyou.tang@gmail.com>
2014-12-17 02:15:26 +0000

    1. move yelp search on server side; 2. add procedure to search shops by location and distance in mysql

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -17,28 +17,55 @@ class SearchController extends Zend_Controller_Action
 
     public function searchAction()
     {
-        $result = array();
+        $fyi = '';
+        $status = '';
 
         $term = $this->_getParam('term');
         $location = $this->_getParam('location');
 
-        $yelp = new YelpSearch();
-        $yelp_json = $yelp->search($term, $location);
+        $result = array();
+        try {
+            $yelp = new YelpSearch();
+            $yelp_json = $yelp->search($term, $location);
 
-        $yelp_array = Zend_Json::decode($yelp_json);
-        $yelp_businesses = $yelp_array['businesses'];
+            $yelp_array = Zend_Json::decode($yelp_json);
+            $yelp_businesses = $yelp_array['businesses'];
 
-        foreach ($yelp_businesses as $business) {
-            $shop_id = $business['id'];
-            $name = $business['name'];
-            $image_url = $business['image_url'];
-            $business_map = array('shop_id' => $shop_id,
-                                  'name' => $name,
-                                  'image_url' => $image_url);
-            $result[] = $business_map;
+            foreach ($yelp_businesses as $business) {
+                $shop_id = 'yelp::' . $business['id'];
+                $name = $business['name'];
+                $phone = $business['phone'];
+                $shop_location = $business['location'];
+                $street = $shop_location['address'][0];
+                $city = $shop_location['city'];
+                $state = $shop_location['state_code'];
+                $zip_code = $shop_location['postal_code'];
+                $country = $shop_location['country_code'];
+                $coordinate = $shop_location['coordinate'];
+                $latitude = $coordinate['latitude'];
+                $longitude = $coordinate['longitude'];
+                $image_url = $business['image_url'];
+                $business_map = array('shop_id' => $shop_id,
+                                      'name' => $name,
+                                      'phone' => $phone,
+                                      'image_url' => $image_url,
+                                      'street' => $street,
+                                      'city' => $city,
+                                      'state' => $state,
+                                      'zip_code' => $zip_code,
+                                      'country' => $country,
+                                      'latitude' => $latitude,
+                                      'longitude' => $longitude
+                );
+                $result[] = $business_map;
+            }
+
+            $status = $result;
+        }catch(Exception $e){
+            $status = 'exception[' .  $e->getMessage() . ']';
         }
 
-        echo Zend_Json::encode(array('term'=>$term, 'location'=>$location, 'result'=>$result));
+        echo Zend_Json::encode(array('fyi'=>$fyi, 'status'=>$status));
         exit();
     }
 }
