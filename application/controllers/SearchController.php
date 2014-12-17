6ea4dc3d4de17cf22b50f407a0d10e2382c5bb69
C.Y. Tang <chuanyou.tang@gmail.com>
2014-12-16 20:22:56 -0500

    reoganize search functions

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -1,19 +1,5 @@
 <?php
 
-require_once('OAuth.php');
-
-$GLOBALS = array(
-'CONSUMER_KEY' => 'At0JvIC6ipoyBDlkLkdFfg',
-'CONSUMER_SECRET' => 'N-5hqnGE-Z0HorComNGl1oe90Yk',
-'TOKEN' => '0T3SB6CpDFR60Ua9c6rOrwmGsvGrTSi2',
-'TOKEN_SECRET' => 'QIBmqY3Bj28pPmoq45-bNuKNtas',
-'API_HOST' => 'api.yelp.com',
-'DEFAULT_TERM' => 'dinner',
-'DEFAULT_LOCATION' => 'San Francisco, CA',
-'SEARCH_LIMIT' => 3,
-'SEARCH_PATH' => '/v2/search/',
-'BUSINESS_PATH' => '/v2/business/');
-
 class SearchController extends Zend_Controller_Action
 {
 
@@ -33,7 +19,9 @@ class SearchController extends Zend_Controller_Action
 
         $term = $this->_getParam('term');
         $location = $this->_getParam('location');
-        $yelp_json = $this->search($term, $location);
+        
+        $yelp = new YelpSearch();
+        $yelp_json = $yelp_search->search($term, $location);
 
         $yelp_array = Zend_Json::decode($yelp_json);
         $yelp_businesses = $yelp_array['businesses'];
@@ -51,64 +39,5 @@ class SearchController extends Zend_Controller_Action
         echo Zend_Json::encode(array('term'=>$term, 'location'=>$location, 'result'=>$result));
         exit();
     }
-
-/** 
- * Makes a request to the Yelp API and returns the response
- * 
- * @param    $host    The domain host of the API 
- * @param    $path    The path of the APi after the domain
- * @return   The JSON response from the request      
- */
-function request($host, $path) {
-    $unsigned_url = "http://" . $host . $path;
-    // Token object built using the OAuth library
-    $token = new OAuthToken($GLOBALS['TOKEN'], $GLOBALS['TOKEN_SECRET']);
-    // Consumer object built using the OAuth library
-    $consumer = new OAuthConsumer($GLOBALS['CONSUMER_KEY'], $GLOBALS['CONSUMER_SECRET']);
-    // Yelp uses HMAC SHA1 encoding
-    $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
-    $oauthrequest = OAuthRequest::from_consumer_and_token(
-        $consumer, 
-        $token, 
-        'GET', 
-        $unsigned_url
-    );
-    
-    // Sign the request
-    $oauthrequest->sign_request($signature_method, $consumer, $token);
-    
-    // Get the signed URL
-    $signed_url = $oauthrequest->to_url();
-    
-    // Send Yelp API Call
-    $ch = curl_init($signed_url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    
-    return $data;
-}
-
-/**
- * Query the Search API by a search term and location 
- * 
- * @param    $term        The search term passed to the API 
- * @param    $location    The search location passed to the API 
- * @return   The JSON response from the request 
- */
-function search($term, $location) {
-    $url_params = array();
-    
-    $url_params['term'] = $term ?: $GLOBALS['DEFAULT_TERM'];
-    $url_params['location'] = $location?: $GLOBALS['DEFAULT_LOCATION'];
-    $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
-    $search_path = $GLOBALS['SEARCH_PATH'] . "?" . http_build_query($url_params);
-    
-    return $this->request($GLOBALS['API_HOST'], $search_path);
-}
-
-
-
 }
 
---
+++ library/YelpSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+require_once('OAuth.php');
+
+$GLOBALS = array(
+'CONSUMER_KEY' => 'At0JvIC6ipoyBDlkLkdFfg',
+'CONSUMER_SECRET' => 'N-5hqnGE-Z0HorComNGl1oe90Yk',
+'TOKEN' => '0T3SB6CpDFR60Ua9c6rOrwmGsvGrTSi2',
+'TOKEN_SECRET' => 'QIBmqY3Bj28pPmoq45-bNuKNtas',
+'API_HOST' => 'api.yelp.com',
+'DEFAULT_TERM' => 'dinner',
+'DEFAULT_LOCATION' => 'San Francisco, CA',
+'SEARCH_LIMIT' => 3,
+'SEARCH_PATH' => '/v2/search/',
+'BUSINESS_PATH' => '/v2/business/');
+
+class YelpSearch
+{
+    /** 
+     * Makes a request to the Yelp API and returns the response
+     * 
+     * @param    $host    The domain host of the API 
+     * @param    $path    The path of the APi after the domain
+     * @return   The JSON response from the request      
+     */
+    function request($host, $path) {
+        $unsigned_url = "http://" . $host . $path;
+        // Token object built using the OAuth library
+        $token = new OAuthToken($GLOBALS['TOKEN'], $GLOBALS['TOKEN_SECRET']);
+        // Consumer object built using the OAuth library
+        $consumer = new OAuthConsumer($GLOBALS['CONSUMER_KEY'], $GLOBALS['CONSUMER_SECRET']);
+        // Yelp uses HMAC SHA1 encoding
+        $signature_method = new OAuthSignatureMethod_HMAC_SHA1();
+        $oauthrequest = OAuthRequest::from_consumer_and_token(
+            $consumer, 
+            $token, 
+            'GET', 
+            $unsigned_url
+        );
+
+        // Sign the request
+        $oauthrequest->sign_request($signature_method, $consumer, $token);
+
+        // Get the signed URL
+        $signed_url = $oauthrequest->to_url();
+
+        // Send Yelp API Call
+        $ch = curl_init($signed_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
+    }
+
+    /**
+     * Query the Search API by a search term and location 
+     * 
+     * @param    $term        The search term passed to the API 
+     * @param    $location    The search location passed to the API 
+     * @return   The JSON response from the request 
+     */
+    function search($term, $location) {
+        $url_params = array();
+
+        $url_params['term'] = $term ?: $GLOBALS['DEFAULT_TERM'];
+        $url_params['location'] = $location?: $GLOBALS['DEFAULT_LOCATION'];
+        $url_params['limit'] = $GLOBALS['SEARCH_LIMIT'];
+        $search_path = $GLOBALS['SEARCH_PATH'] . "?" . http_build_query($url_params);
+
+        return $this->request($GLOBALS['API_HOST'], $search_path);
+    }
+}
